db40c2a347c87d33eb7646d384abb56e0b13ca6a
abcd2745 <abcd2745@sohu.com>
2019-10-21 22:35:23 +0800

    gateway--demo
    Signed-off-by: abcd2745 <abcd2745@sohu.com>

---
+++ micro/gateway/demo/src/test/java/com/lc/example/micro/gateway/demo/GatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lc.example.micro.gateway.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
