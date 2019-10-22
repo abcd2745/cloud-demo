b8ad1b4a28cb0468007a4ebb00dcfde97422caa2
abcd2745 <abcd2745@sohu.com>
2019-10-22 17:11:09 +0800

    gateway

--- micro/gateway/demo/src/main/java/com/lc/example/micro/gateway/demo/GatewayRoutes.java
+++ micro/gateway/demo/src/main/java/com/lc/example/micro/gateway/demo/GatewayRoutes.java
@@ -1,18 +1,21 @@
 package com.lc.example.micro.gateway.demo;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Configurable
+@Configuration
 public class GatewayRoutes {
 
-//    @Bean
-//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route("path_route", r -> r.path("/get")
-//                        .uri("http://httpbin.org"))
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        System.out.println("111111111111");
+
+        return builder.routes()
+                .route("path_route", r -> r.path("/java/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://localhost:8090/demo.json"))
 //                .route("host_route", r -> r.host("*.myhost.org")
 //                        .uri("http://httpbin.org"))
 //                .route("rewrite_route", r -> r.host("*.rewrite.org")
@@ -28,19 +31,16 @@ public class GatewayRoutes {
 //                        .host("*.limited.org").and().path("/anything/**")
 //                        .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
 //                        .uri("http://httpbin.org"))
-//                .build();
-//    }
-//
-    @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r ->
-                        r.path("/java/**")
-                                .filters(
-                                        f -> f.stripPrefix(1)
-                                )
-                                .uri("http://localhost:8090/demo.json")
-                )
                 .build();
     }
 }
+//
+//    @Bean
+//    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+//        System.out.println("1111");
+//        return builder.routes()
+//                .route(r -> r.path("/java/**")
+////                        .filters(f -> f.stripPrefix(1))
+//                        .uri("http://localhost:8090/demo.json")
+//                )
+//        
