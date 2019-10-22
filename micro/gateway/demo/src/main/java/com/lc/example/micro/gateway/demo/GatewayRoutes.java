3ea49c4c460d284aaa89242e3aca0e953d87d578
abcd2745 <abcd2745@sohu.com>
2019-10-22 17:13:34 +0800

    '1'

--- micro/gateway/demo/src/main/java/com/lc/example/micro/gateway/demo/GatewayRoutes.java
+++ micro/gateway/demo/src/main/java/com/lc/example/micro/gateway/demo/GatewayRoutes.java
@@ -10,37 +10,10 @@ public class GatewayRoutes {
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        System.out.println("111111111111");
-
         return builder.routes()
                 .route("path_route", r -> r.path("/java/**")
                         .filters(f -> f.stripPrefix(1))
                         .uri("http://localhost:8090/demo.json"))
-//                .route("host_route", r -> r.host("*.myhost.org")
-//                        .uri("http://httpbin.org"))
-//                .route("rewrite_route", r -> r.host("*.rewrite.org")
-//                        .filters(f -> f.rewritePath("/foo/(?<segment>.*)", "/${segment}"))
-//                        .uri("http://httpbin.org"))
-//                .route("hystrix_route", r -> r.host("*.hystrix.org")
-//                        .filters(f -> f.hystrix(c -> c.setName("slowcmd")))
-//                        .uri("http://httpbin.org"))
-//                .route("hystrix_fallback_route", r -> r.host("*.hystrixfallback.org")
-//                        .filters(f -> f.hystrix(c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback")))
-//                        .uri("http://httpbin.org"))
-//                .route("limit_route", r -> r
-//                        .host("*.limited.org").and().path("/anything/**")
-//                        .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
-//                        .uri("http://httpbin.org"))
                 .build();
     }
 }
-//
-//    @Bean
-//    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-//        System.out.println("1111");
-//        return builder.routes()
-//                .route(r -> r.path("/java/**")
-////                        .filters(f -> f.stripPrefix(1))
-//                        .uri("http://localhost:8090/demo.json")
-//                )
-//        
