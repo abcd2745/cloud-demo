5ba377ba100443ef819306b929e868f51a0cc5c1
abcd2745 <abcd2745@sohu.com>
2019-11-03 18:40:16 +0800

    actuator配置
    Signed-off-by: abcd2745 <abcd2745@sohu.com>

--- demo/src/main/java/com/lc/cloud/demo/DemoApplication.java
+++ demo/src/main/java/com/lc/cloud/demo/DemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+/**
+ * https://docs.spring.io/spring-boot/docs/current/reference/html/index.html
+ */
 @SpringBootApplication
 @EnableConfigurationProperties
 public class DemoApplication {
