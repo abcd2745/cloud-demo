fc5c9ee702a944db8e87963e1c30d89e475980b7
abcd2745 <abcd2745@sohu.com>
2019-11-07 23:14:00 +0800

    '1'

--- micro/eureka/microservice-simple-provider-user/src/main/java/com/lc/example/micro/provider/controller/UserController.java
+++ micro/eureka/microservice-simple-provider-user/src/main/java/com/lc/example/micro/provider/controller/UserController.java
@@ -3,6 +3,7 @@ package com.lc.example.micro.provider.controller;
 import com.lc.example.micro.provider.dao.UserRepository;
 import com.lc.example.micro.provider.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/{id}")
     public User findById(@PathVariable Long id) {
